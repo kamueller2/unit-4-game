c0c925902190624771407c2459674b3f381be672
Kimberly <kamueller2@gmail.com>
2019-06-05 00:09:18 -0400

    created new variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,55 @@ $(document).ready(() => {
 
     //variables//
 
-    let randomNum = [];
-    let crystalNum = [];
-    let totalScore = 0;
-    let wins = 0;
-    let losses = 0;
+    let crystal = {
+        diamond: {
+            name: 'Diamond',
+            value: 0
+        },
+        pink: {
+            name: 'Pink',
+            value: 0
+        },
+        purple: {
+            name: 'Purple',
+            value = 0
+        },
+        blue: {
+            name: 'Blue',
+            value = 0
+        }
+    }
 
+    //scores//
+    let currentScore = 0;
+    let targetScore = 0;
+
+    //wins & losses//
+    let winCount = 0;
+    let lossCoount = 0;
 });
+
+// let randomNum = [];
+// let crystalNum = [];
+// let totalScore = 0;
+// let wins = 0;
+// let losses = 0;
+
+// //arrays to select randomNum & crystalNum//
+
+// for (let i = 19; i < 121; i++) {
+//     randomNum.push(i);
+// }
+
+// for (let i = 1; i < 13; i++) {
+//     crystalNum.push(i);
+// }
+
+// //global variables//
+
+// let compNum = 0;
+
+// //functions//
+
+// let comp = randomNum[Math.floor(Math.random) * randomNum.length];
+// compNum = comp;
