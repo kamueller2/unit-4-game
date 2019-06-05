bac0ffc2f122a95e9dcc7167dd7dfe1da9baf4a9
Kimberly Mueller <kamueller2@gmail.com>
2019-06-05 19:58:07 -0400

    created click functions for dogs and tested

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,59 @@
-$(document).ready(() => {
+// $(document).ready(() => {
 
-    //variables//
+//variables//
 
-    let dog = {
-        pom: {
-            name: 'Pom',
-            value: 0
-        },
-        chi: {
-            name: 'Chi',
-            value: 0
-        },
-        pup: {
-            name: 'Pup',
-            value = 0
-        },
-        pug: {
-            name: 'Pug',
-            value = 0
-        }
+let dog = {
+    pom: {
+        name: 'Pom',
+        value: 0
+    },
+    chi: {
+        name: 'Chi',
+        value: 0
+    },
+    pup: {
+        name: 'Pup',
+        value: 0
+    },
+    pug: {
+        name: 'Pug',
+        value: 0
     }
+}
 
-    //scores//
-    let currentScore = 0;
-    let targetScore = 0;
+//scores
+let currentScore = 0;
+let targetScore = 0;
 
-    //wins & losses//
-    let winCount = 0;
-    let lossCoount = 0;
+//wins & losses
+let winCount = 0;
+let lossCoount = 0;
 
 
-    //jQuery//
+//jQuery
 
-    $('#pom').click(function() {
-        alert("test");
-    })
+$('#pom').click(function() {
+    // test
+    //alert("test");
+})
 
-});
+$('#chi').click(function() {
+    // test
+    // alert("test");
+})
 
-// let randomNum = [];
-// let crystalNum = [];
-// let totalScore = 0;
-// let wins = 0;
-// let losses = 0;
+$('#pup').click(function() {
+    // test
+    // alert("test");
+})
 
-// //arrays to select randomNum & crystalNum//
+$('#pug').click(function() {
+    // test
+    // alert("test");
+})
 
-// for (let i = 19; i < 121; i++) {
-//     randomNum.push(i);
-// }
 
-// for (let i = 1; i < 13; i++) {
-//     crystalNum.push(i);
-// }
 
-// //global variables//
 
-// let compNum = 0;
 
-// //functions//
-
-// let comp = randomNum[Math.floor(Math.random) * randomNum.length];
-// compNum = comp;
+// });
